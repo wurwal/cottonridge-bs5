bb4c36f7cbd45309c6a919624938af8d2a8ba81d
Thomas Wurwal <wurwal@gmail.com>
2024-09-25 17:43:39 +0100

    updates 25/9/24

--- assets/app.js
+++ assets/app.js
@@ -4,3 +4,15 @@ var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
     'customClass': 'cottonridge-tooltip'
   })
 })
+
+
+document.querySelector('.link-more').addEventListener('click', function() {
+    const moreContent = document.querySelector('.more');
+    if (moreContent.style.display === 'none') {
+        moreContent.style.display = 'block';
+        this.textContent = 'Less info...';
+    } else {
+        moreContent.style.display = 'none';
+        this.textContent = 'More info...';
+    }
+});
