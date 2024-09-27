13a3fc265e25b5705e19dc9d7dd8a8f602cea665
Thomas Wurwal <wurwal@gmail.com>
2024-09-27 16:38:04 +0100

    product gallery and hot spot features added

--- assets/app.js
+++ assets/app.js
@@ -1,18 +1,33 @@
 var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
 var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
   return new bootstrap.Tooltip(tooltipTriggerEl, {
-   
+
   })
 })
 
 
-document.querySelector('.link-more').addEventListener('click', function() {
-    const moreContent = document.querySelector('.more');
-    if (moreContent.style.display === 'none') {
-        moreContent.style.display = 'block';
-        this.textContent = 'Less info...';
-    } else {
-        moreContent.style.display = 'none';
-        this.textContent = 'More info...';
+//thumbnail nav
+var swiper = new Swiper(".swiper-1", {
+      spaceBetween: 10,
+      slidesPerView: 3,
+      freeMode: true,
+      watchSlidesProgress: true,
+      direction: "horizontal",
+      breakpoints: {
+      767: {
+        direction: "vertical",
+      }
     }
+});
+//main photo
+    var swiper2 = new Swiper(".swiper-2", {
+      spaceBetween: 10,
+      zoom:true,
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+      thumbs: {
+        swiper: swiper,
+      },
 });
